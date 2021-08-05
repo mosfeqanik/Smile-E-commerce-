8159cbdccdb70f90adcfb87409b5ff830c5cec75
mosfeqanik <mosfeqanik01@gmail.com>
2021-08-05 19:25:11 +0600

    model view controller added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(Smile());
 
-class MyApp extends StatelessWidget {
-
+class Smile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container();
   }
 }
-
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:smile/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Smile());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
